bdf993976672780625ee59b1f091dbcb0842db8d
Alex Ramírez <alexis.rj2110@gmail.com>
2023-01-07 13:22:07 -0500

    Subida2

--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -16,6 +16,7 @@ public class Problema7 {
     public static void main(String[] args) {
         String[] ciudades;
         ciudades = funcionArreglo();
+        procedimiento(ciudades);
     }
 
     public static String[] funcionArreglo() {
@@ -26,10 +27,23 @@ public class Problema7 {
         System.out.println("Ingrese el tamaño del arreglo:");
         tamano = entrada.nextInt();
         String[] ciudades = new String[tamano];
+        entrada.nextLine();
+        int contador = 1;
         
-        for (int i = 1; i <= tamano; i++) {
-            System.out.printf("Ingrese la ciudad número %d",i);
+        for (int i = 0; i < tamano; i++) {
+            System.out.printf("Ingrese la ciudad número %d:\n",contador);
             ciudades[i] = entrada.nextLine();
         }
+        
+        return ciudades;
+    }
+    public static void procedimiento(String[] ciudades){
+        String mensaje = "";
+        for (int i = 0; i < ciudades.length; i++) {
+            if (ciudades[i].length() == 5 || ciudades[i].length() == 4  ) {
+                mensaje = String.format("%s\n%s",mensaje,ciudades[i]);
+            }
+        }
+        System.out.printf("Las ciudades de 4 a o 5 caracteres son:%s",mensaje);
     }
 }
